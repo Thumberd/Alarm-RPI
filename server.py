f987939ffc3a3fc32080b5a84fad8c3be59f53d1
Albrecht Jérémy <ajeremyalbrecht@gmail.com>
2016-06-18 08:24:19 +0200

    Refactoring

--- server.py
+++ server.py
@@ -1,69 +1,48 @@
-#Import logging tool
-import logging
-from logging.handlers import RotatingFileHandler
-
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-#Define formatter
-formatter = logging.Formatter('%(asctime)s :: [%(levelname)s] %(message)s')
-
-#1st handler for file writing
-file_handler = RotatingFileHandler('/home/pi/System/logs/server.log', 'a', 1000000, 1)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-#2nd handler for console
-steam_handler = logging.StreamHandler()
-steam_handler.setLevel(logging.DEBUG)
-logger.addHandler(steam_handler)
-#Logging config done
-
+# Import standard library
 import socket
 from Crypto.PublicKey import RSA
-from MySQLhandler import MySQL
 import os
 import sqlite3
 import signal
+
+# Application import
 from AlarmClass import Alarm
+from MySQLhandler import MySQL
+import Utility
 
-f = open('/home/pi/System/pri_key', 'r')
-key = RSA.importKey(f.read())
-f.close()
+SCRIPT_NAME = "server"
 
-#Defining DB connection
-db = MySQL('devices')
-DBalarm = MySQL('alarms')
+logger = Utility.initialize_logger(SCRIPT_NAME)
 
-#getting camera PID
-pidDB = sqlite3.connect('/home/pi/System/PID.db')
-pidCursor = pidDB.cursor()
-pidCursor.execute("""SELECT value FROM PID WHERE name = 'camera'""")
-camera = pidCursor.fetchone()[0]
+# Defining DB connection
+db_devices = MySQL('devices')
+db_alarms = MySQL('alarms')
 
-ip = "192.168.0.17"
-port = 5400
+# Get camera PID
+camera_pid = Utility.get_camera_PID()
+
+IP = "192.168.0.17"
+PORT = 5400
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((ip, port))
-print("Server is listening on " + str(ip) +":" + str(port))
-
+server.bind((IP, PORT))
+logger.info("Server is listening on {}:{}".format(IP, PORT))
 server.listen(5)
 
 while True:
-        client, address = server.accept()
-        print("Client connected on " + str(address))
-        response = client.recv(255)
-        res = key.decrypt(response)
-	code = res[0:2]
-	device = db.get('code', code)[0]
-	if device:
-		print(device['name'])
-		if res[2:] == "ALARM":
-			deviceAlarm = DBalarm.get('device_id', device['id'])[0]
-			if deviceAlarm['state'] == 1:
-				print("Alarme")
-				Alarm(device['id']).MotionProtocol()
-	else:
-		print(res)
+    client, address = server.accept()
+    logger.debug("Client connected from {]".format(address))
+    response = client.recv(255)
+    code = response[0:2]
+    device = db_devices.get('code', code)[0]
+    if device:
+        logger.debug("Client name: {}".format(device['name']))
+        if response[2:] == "ALARM":
+            deviceAlarm = db_alarms.get('device_id', device['id'])[0]
+            if deviceAlarm['state'] == 1:
+                print("Alarme")
+                Alarm(device['id']).MotionProtocol()
+    else:
+        print(res)
 print("Close")
 client.close()
 stock.close()
--- worker.py
+++ worker.py
@@ -1,13 +1,19 @@
+# Standard library imports
+import socket
+
+# Third party imports
 from celery import Celery
 from celery.schedules import crontab
 from celery.decorators import periodic_task
 
+# Application imports
 from MySQLhandler import MySQL
 import Utility
 
 from datetime import datetime
 
 SCRIPT_NAME = "celery"
+SALT = "first"
 
 logger = Utility.initialize_logger(SCRIPT_NAME)
 
@@ -16,32 +22,29 @@ alarms = MySQL('alarms')
 
 celery = Celery('worker', broker='amqp://guest:guest@localhost')
 
-def sendTo(ip, msg):
-    f = open('/home/pi/System/up_pub_key', 'r')
-    UPkey = RSA.importKey(f.read())
-    f.close()
 
+def send_to(ip, msg):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect((ip, 5400))
-        s.send(UPkey.encrypt('10' + msg, 32)[0])
+        s.send(SALT + msg)
         s.close()
     except:
-        logger.error("Erreur")
+        logger.error("Error: ")
 
 
 @periodic_task(run_every=crontab(hour='*', minute='*'))
 def check_for_alarm_scheduled():
-	now = datetime.now()
-	hour = now.hour
-	minute = now.minute
-	for aScheduled in scheduled.all():
-		if int(aScheduled['beginHour']) == hour and int(aScheduled['beginMinute']) == minute:
-			alarm = alarms.get('id', aScheduled['alarm_id'])[0]
-			alarms.modify(alarm['id'], 'state', True)
-		elif int(aScheduled['endHour']) == hour and int(aScheduled['endMinute']) == minute:
-			alarm = alarms.get('id', aScheduled['alarm_id'])[0]
-                        alarms.modify(alarm['id'], 'state', False)
+    now = datetime.now()
+    hour = now.hour
+    minute = now.minute
+    for aScheduled in scheduled.all():
+        if int(aScheduled['beginHour']) == hour and int(aScheduled['beginMinute']) == minute:
+            alarm = alarms.get('id', aScheduled['alarm_id'])[0]
+            alarms.modify(alarm['id'], 'state', True)
+        elif int(aScheduled['endHour']) == hour and int(aScheduled['endMinute']) == minute:
+            alarm = alarms.get('id', aScheduled['alarm_id'])[0]
+            alarms.modify(alarm['id'], 'state', False)
 
 
 @periodic_task(run_every=crontab(hour='*', minute='*'))
@@ -60,13 +63,13 @@ def check_for_alarm_notifications():
     alarm_up = db_alarms.get('state', 1)
     if alarm_up:
         for alarm in alarm_up:
-            #Get the time since the alarm's state changed
+            # Get the time since the alarm's state changed
             duree = datetime.now() - alarm['updated_at']
             if duree.total_seconds() < 120.0:
                 device = db_devices.get('id', alarm['device_id'])[0]
                 if device['ip'] != "":
-                    sendTo(device['ip'], "STATE1")
-
+                    send_to(device['ip'], "STATE1")
+    # Get all the alarm which are currently OFF
     alarmDOWN = db_alarms.get('state', 0)
     if alarmDOWN:
         for alarm in alarmDOWN:
@@ -74,4 +77,4 @@ def check_for_alarm_notifications():
             if duree.total_seconds() < 120.0:
                 device = db_devices.get('id', alarm['device_id'])[0]
                 if device['ip'] != "":
-                    sendTo(device['ip'], "STATE0")
+                    send_to(device['ip'], "STATE0")
